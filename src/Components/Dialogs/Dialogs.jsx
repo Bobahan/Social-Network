d512af43597bd5fcd1f12b92bac4168f672eb73b
Bobahan <bobahansem98@icloud.com>
2022-10-27 09:40:58 +0300

    add Dialogs pages and add users

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,10 +1,32 @@
 import React from "react";
-import style from './Dialogs.module.css';
+import s from './Dialogs.module.css';
+import userAvatar from '../../assets/user.jpeg'
+
 
 const Dialogs = () => {
     return (
-        <div>
-            I AM A DIALOGS
+        <div className={s.parentContainer}>
+            <div className={s.users}>
+                <div className={s.item}>
+                    <div style={{ 'textAlign': 'center' }}>Vladimir</div>
+                    <div>
+                        <img src={userAvatar} className={s.imgUser} />
+                    </div>
+                </div>
+                <div className={s.item}>
+                    <div style={{ 'textAlign': 'center' }}>Vladimir</div>
+                    <div>
+                        <img src={userAvatar} className={s.imgUser} />
+                    </div>
+                </div>
+                <div className={s.item}>
+                    <div style={{ 'textAlign': 'center' }}>Vladimir</div>
+                    <div>
+                        <img src={userAvatar} className={s.imgUser} />
+                    </div>
+                </div>
+            </div>
+            <div className={s.item} style={{'padding': '50px'}}>Messages</div>
         </div>
     )
 }
