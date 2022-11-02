d5528cb75ebe640e2d3d2345a534f56a7221231d
Bobahan <bobahansem98@icloud.com>
2022-11-02 09:55:20 +0300

    mount a users component

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -4,17 +4,29 @@ import axios from "axios";
 
 // У Классовой Компоненты есть как минимум свойство render()
 // и основная задача это вернуть JSX
+// render возвращает JSX 
+
+// в созданном объекте this будут лежать props
+// в первую очередь инициализация затем render JSX
+// кстати мы не создаем объект от Класса. Вместо нас этим занимается React
+
 class Users extends React.Component {
-    constructor(props) {
-        super(props)
+
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => {
                 this.props.setUsers(response.data.items)
             })
     }
+
     render() {
         return (
             <div>
+                <div>
+                    <span>1</span>
+                    <span>2</span>
+                    <span>3</span>
+                </div>
                 {
                     this.props.users.map(u =>
                         <div key={u.id}>
