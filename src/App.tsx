84afa2a6a1a6250ebbfaea6168a176de7861205c
Bobahan <bobahansem98@icloud.com>
2022-12-21 12:23:27 +0300

    fix app comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,92 +1,43 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 
-import { Routes, Route, BrowserRouter, Navigate, NavLink, useParams, Link } from 'react-router-dom';
-import { Provider, useSelector } from 'react-redux';
-import { compose } from 'redux';
-import { connect } from 'react-redux';
+import { Routes, Route, Navigate, Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
-import Navbar from './Components/Navbar/Navbar';
 import { UsersPage } from './Components/Users/UsersContainer';
 import { Login } from './Components/Login/Login';
-import { withRouter } from './Components/HOC/withRouter';
 import Preloader from './Components/Common/Preloader/Preloader';
-import store, { AppStateType } from './redux/redux-store';
+import { AppStateType, DispatchType } from './redux/redux-store';
 import { withSuspense } from './Components/HOC/withSuspense';
-import { actionsApp } from './redux/app-reducer';
+import { initializeApp } from './redux/app-reducer';
 
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { Layout, Menu, theme } from 'antd';
 import MenuItem from 'antd/es/menu/MenuItem';
 import SubMenu from 'antd/es/menu/SubMenu';
 import { Header } from './Components/Header/Header';
 import { withAuthRedirect } from './Components/HOC/withAuthRedirect';
-// import ProfileContainer from './Components/Profile/ProfileContainer';
-// import { ChatPage } from './pages/Chat/ChatPage';
+
 
 const DialogsContainer = withSuspense(React.lazy(() => import('./Components/Dialogs/DialogsContainer')))
 const ProfileContainer = withSuspense(React.lazy(() => import('./Components/Profile/ProfileContainer')))
 const ChatPage = withSuspense(withAuthRedirect(React.lazy(() => import('./pages/Chat/ChatPage'))))
 
-// class App extends React.Component<MapStateToPropsType & MapDispatchToProps> {
-//   componentDidMount() {
-//     this.props.initializeApp()
-//   }
-
-//   render() {
-//     if (!this.props.initialized) {
-//       return <Preloader />
-//     }
-//     return (
-//       <div className='app-wrapper'>
-//         <HeaderContainer />
-//         <Navbar />
-//         <div className='app-wrapper-content'>
-//           <Routes>
-//             <Route path='/' element={<Navigate to={'/profile'} />} />
-//             <Route path='/profile' element={<ProfileContainer />}>
-//               <Route path=':userId' element={<ProfileContainer />} />
-//             </Route>
-//             <Route path='/dialogs' element={<DialogsContainer />} />
-//             <Route path='/users' element={<UsersPage />} />
-//             <Route path='/login' element={<Login />} />
-//             <Route path='*' element={<div style={{ 'display': 'flex', "justifyContent": 'center', 'alignItems': 'center', 'height': '100%', 'fontWeight': '700' }}>404 NOT FOUND</div>} />
-//           </Routes>
-//         </div>
-//       </div>
-//     )
-//   }
-// }
-
-// const mapStateToProps = (state: AppStateType): MapStateToPropsType => {
-//   return {
-//     initialized: state.app.initialized,
-//   }
-// }
-
-// const AppContainer = compose<React.ComponentType>(
-//   withRouter,
-//   connect<MapStateToPropsType, MapDispatchToProps, {}, AppStateType>(mapStateToProps, { initializeApp: actionsApp.initializeApp })
-// )(App)
-
-// type MapStateToPropsType = { initialized: boolean }
-// type MapDispatchToProps = { initializeApp: () => void }
-
-// export const MainApp: React.FC = () => {
-//   return (
-//     <BrowserRouter >
-//       <Provider store={store}>
-//         <AppContainer />
-//       </Provider>
-//     </BrowserRouter>
-//   )
-// }
-
 const { Content, Sider } = Layout;
 
-//@ts-ignor
 export const MainApp: React.FC = () => {
   const { token: { colorBgContainer }, } = theme.useToken()
-  const login = useSelector((state: AppStateType) => state.auth.login)
+  const initialized = useSelector((state: AppStateType) => state.app.initialized)
+
+  const dispatch = useDispatch<DispatchType>()
+
+  useEffect(() => {
+    dispatch(initializeApp())
+  }, [])
+
+  if (!initialized) {
+    return <Preloader />
+  }
+
   return (
     <Layout>
       <Header />
@@ -103,11 +54,6 @@ export const MainApp: React.FC = () => {
           </Menu>
         </Sider>
         <Layout style={{ padding: '0 24px 24px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
           <Content style={{ padding: 24, margin: 0, minHeight: 280, background: colorBgContainer, }}>
             <Routes>
               <Route path='/' element={<Navigate to={'/profile'} />} />
@@ -117,7 +63,7 @@ export const MainApp: React.FC = () => {
               <Route path='/dialogs' element={<DialogsContainer />} />
               <Route path='/users' element={<UsersPage />} />
               <Route path='/login' element={<Login />} />
-              <Route path='*' element={<div style={{ 'display': 'flex', "justifyContent": 'center', 'alignItems': 'center', 'height': '100%', 'fontWeight': '700' }}>404 NOT FOUND</div>} />
+              <Route path='*' element={<div className='error'>404 NOT FOUND</div>} />
               <Route path='/chat' element={<ChatPage />} />
             </Routes>
           </Content>
