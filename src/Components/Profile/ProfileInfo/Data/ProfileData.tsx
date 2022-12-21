55f00cc22adb28484a59045491d7d1674657ea80
Bobahan <bobahansem98@icloud.com>
2022-12-21 10:56:10 +0300

    refactor profileData

--- src/Components/Profile/ProfileInfo/Data/ProfileData.tsx
+++ src/Components/Profile/ProfileInfo/Data/ProfileData.tsx
@@ -1,18 +1,23 @@
 import React from "react";
-import { ContactsType, ProfileType } from "../../../../types/types";
 import Contacts from "../Contacts/ProfileContacts";
+import style from './ProfileData.module.css';
+
+import { ContactsType, ProfileType } from "../../../../types/types";
 
 const ProfileData: React.FC<ProfileDataType> = (props) => {
     return (
-        <div style={{ 'marginTop': '10px' }}>
+        <div className={style.profileBlock}>
             {props.isOwner && <div><button onClick={props.activateEditMode}>Edit</button></div>}
-            <div><span style={{ 'fontWeight': '700', 'borderBottom': '1px solid black' }}>AboutMe: </span><span style={{ 'alignSelf': 'center' }}>{props.profile.aboutMe}</span></div>
-            <div><span style={{ 'fontWeight': '700', 'borderRadius': '5%' }}>FullName:</span> <span style={{ 'alignSelf': 'center' }}>{props.profile.fullName}</span></div>
-            <div><span style={{ 'fontWeight': '700', 'borderRadius': '5%' }}>UserID:</span> <span>{props.profile.userId}</span></div>
-            <div><span style={{ 'fontWeight': '700', 'borderRadius': '5%' }}>Looking For A Job Description:</span> <span style={{ 'alignSelf': 'center' }}>{props.profile.lookingForAJobDescription}</span></div>
-            <div><span style={{ 'fontWeight': '700', 'borderRadius': '5%' }}>Contacts:</span> <span>{Object.keys(props.profile.contacts).map(key => {
-                return <Contacts key={key} contactKey={key} contactValue={props.profile.contacts[key as keyof ContactsType]} />
-            })}</span></div>
+            <div><span >AboutMe:</span><span>{props.profile.aboutMe}</span></div>
+            <div><span>FullName:</span><span>{props.profile.fullName}</span></div>
+            <div><span>UserID:</span><span>{props.profile.userId}</span></div>
+            <div><span>Looking For A Job Description:</span><span>{props.profile.lookingForAJobDescription}</span></div>
+            <div>
+                <span>Contacts:</span>
+                <span>{Object.keys(props.profile.contacts).map(key => {
+                    return <Contacts key={key} contactKey={key} contactValue={props.profile.contacts[key as keyof ContactsType]} />
+                })}</span>
+            </div>
         </div>
     )
 }
