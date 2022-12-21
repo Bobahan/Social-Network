40af82ec5253bc8cbddf6e765eb00ae3516f6df5
Bobahan <bobahansem98@icloud.com>
2022-12-21 11:33:24 +0300

    profiledataform fix

--- src/Components/Profile/ProfileInfo/Data/ProfileDataForm.tsx
+++ src/Components/Profile/ProfileInfo/Data/ProfileDataForm.tsx
@@ -1,8 +1,10 @@
-import React from "react"
-import { Field, InjectedFormProps, reduxForm } from "redux-form"
-import { ProfileType } from "../../../../types/types"
-import { FormControlSpan } from "../../../Common/FormController/FormControl"
-import styles from '../../../Common/FormController/FormControl.module.css'
+import React from "react";
+import { Field, InjectedFormProps, reduxForm } from "redux-form";
+import { FormControlSpan } from "../../../Common/FormController/FormControl";
+import styles from '../../../Common/FormController/FormControl.module.css';
+import style from './ProfileData.module.css';
+
+import { ProfileType } from "../../../../types/types";
 
 const Input = FormControlSpan('input')
 const Textarea = FormControlSpan('textarea')
@@ -10,34 +12,37 @@ const Textarea = FormControlSpan('textarea')
 const ProfileDataForm: React.FC<InjectedFormProps<ProfileType, ProfileDataFormType> & ProfileDataFormType> = ({ handleSubmit, error, profile }) => {
     return (
         <form onSubmit={handleSubmit}>
-            <div style={{ 'marginTop': '10px' }}><button>Save</button></div>
-            <div style={{ 'display': 'flex' }}>
-                <div style={{ 'fontWeight': '700', 'borderBottom': '1px solid black', 'alignSelf': 'center' }}>AboutMe: </div>
+            <div className={style.saveButtton}><button>Save</button></div>
+
+            <div className={style.profileSave}>
+                <div className={style.aboutMe}>About Me:</div>
                 <Field name="aboutMe" component={Textarea} type="text" placeholder="About Me" style={{ 'marginLeft': '10px' }} />
             </div>
 
-            <div style={{ 'display': 'flex' }}>
-                <div style={{ 'fontWeight': '700', 'borderRadius': '5%', 'alignSelf': 'center' }}>FullName:</div>
+            <div className={style.profileSave}>
+                <div>Fullname:</div>
                 <Field name="fullName" component={Input} type="text" placeholder="Full Name" style={{ 'marginLeft': '10px' }} />
             </div>
 
-            <div style={{ 'display': 'flex' }}>
-                <div style={{ 'fontWeight': '700', 'borderRadius': '5%', 'alignSelf': 'center' }}>UserID:</div>
-                <span style={{ 'marginLeft': '5px' }}>{profile.userId}</span>
+            <div className={style.profileSave}>
+                <div>UserID:</div>
+                <span className={style.userID}>{profile.userId}</span>
             </div>
 
-            <div style={{ 'display': 'flex' }}>
-                <span style={{ 'fontWeight': '700', 'borderRadius': '5%', 'alignSelf': 'center' }}>Looking For A Job Description:</span>
+            <div className={style.profileSave}>
+                <span className={style.lookingForAJob}>Looking for a job:</span>
                 <Field name="lookingForAJobDescription" component={Input} type="text" placeholder="Yes / No" style={{ 'marginLeft': '10px' }} />
             </div>
 
-            <div><span style={{ 'fontWeight': '700', 'borderRadius': '5%' }}>Contacts:</span> <span>{Object.keys(profile.contacts).map(key => {
-                return (
-                    <div key={key}>
-                        <b>{key}: <Field name={`contacts.` + key} placeholder={key} component={Input} type="text" /></b>
-                    </div>
-                )
-            })}</span></div>
+            <div className={style.contactsBlock}>
+                <span>Contacts:</span>
+                <span>{Object.keys(profile.contacts).map(key => {
+                    return (
+                        <div key={key}>
+                            <b>{key}: <Field name={`contacts.` + key} placeholder={key} component={Input} type="text" /></b>
+                        </div>
+                    )
+                })}</span></div>
             {error && <div className={styles.formControlSummaryError} style={{ 'margin': '10px 10px' }}>{error}</div>}
         </form>
     )
