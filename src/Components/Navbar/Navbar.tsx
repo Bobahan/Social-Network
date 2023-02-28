491f6daea0fa210d08fb67a0dff47eadcfbfdbbd
Bobahan <bobahansem98@icloud.com>
2023-02-28 11:09:09 +0300

    fix a little

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,22 +1,30 @@
-import React from "react";
-import style from './Navbar.module.css'
-import { NavLink } from "react-router-dom";
+import style from './Navbar.module.css';
+import { NavLink } from 'react-router-dom';
 
 const LinkActive = ({ isActive }: { isActive: boolean }) => {
-    return isActive ? style.active : style.text
-}
+  return isActive ? style.active : style.text;
+};
 
 const Navbar = () => {
-    return (
-        <div className={style.navbar}>
-            <div className={style.item}><NavLink className={LinkActive} to={"/profile"}>Profile</NavLink></div>
-            <div className={style.item}><NavLink className={LinkActive} to={"/dialogs"}>Dialogs</NavLink></div>
-            <div className={style.item}><NavLink className={LinkActive} to={"/news"}>News</NavLink></div>
-            <div className={style.item}><NavLink className={LinkActive} to={"/music"}>Music</NavLink></div>
-            <div className={style.item}><NavLink className={LinkActive} to={"/users"}>Users</NavLink></div>
-            <div className={style.item}><NavLink className={LinkActive} to={"/setting"}>Setting</NavLink></div>
-        </div>
-    )
-}
+  return (
+    <div className={style.navbar}>
+      <div className={style.item}>
+        <NavLink className={LinkActive} to={'/profile'}>
+          Profile
+        </NavLink>
+      </div>
+      <div className={style.item}>
+        <NavLink className={LinkActive} to={'/users'}>
+          Users
+        </NavLink>
+      </div>
+      <div className={style.item}>
+        <NavLink className={LinkActive} to={'/chat'}>
+          Chat
+        </NavLink>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
