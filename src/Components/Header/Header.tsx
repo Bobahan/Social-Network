d6b5760bea8244f269ec7d756b0a2d07cddb483d
Bobahan <bobahansem98@icloud.com>
2023-02-28 11:09:37 +0300

    detele background

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,37 +1,38 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { Avatar, Button, Layout } from "antd";
-
-import { UserOutlined } from '@ant-design/icons';
-import { useDispatch, useSelector } from "react-redux";
-import { AppStateType, DispatchType } from "../../redux/redux-store";
-import { logout } from "../../redux/auth-reducer";
-import { Link } from "react-router-dom";
+import { AppStateType, DispatchType } from '../../redux/redux-store';
+import { logout } from '../../redux/auth-reducer';
+import style from './Header.module.css';
 
 export const Header: React.FC = () => {
-    const isAuth = useSelector((state: AppStateType) => state.auth.isAuth)
-    const login = useSelector((state: AppStateType) => state.auth.login)
-    const userIMG = useSelector((state: AppStateType) => state.profilePage.profile?.photos.small)
-    const dispatch = useDispatch<DispatchType>()
+  const isAuth = useSelector((state: AppStateType) => state.auth.isAuth);
+  const login = useSelector((state: AppStateType) => state.auth.login);
+  const userIMG = useSelector((state: AppStateType) => state.profilePage.profile?.photos.small);
+  const dispatch = useDispatch<DispatchType>();
 
-    const logoutCallback = () => {
-        dispatch(logout())
-    }
+  const logoutCallback = () => {
+    dispatch(logout());
+  };
 
-    const { Header } = Layout
-    return (
-        <Header className="header" style={{ 'height': '100%' }}>
-            <div style={{ 'display': 'flex', 'justifyContent': 'flex-end' }}>
-                {isAuth ?
-                    <span style={{ 'display': 'flex', 'marginLeft': '10px' }}>
-                        <Avatar style={{ 'float': 'right' }} size={64} icon={<UserOutlined />} />
-                        <span style={{ 'color': 'white' }}>
-                            {login}
-                            <span onClick={logoutCallback} style={{ 'cursor': 'pointer', 'color': 'white', 'marginLeft': '10px' }}><Button>Logout</Button></span>
-                        </span>
-                    </span>
-                    : <Link style={{ 'textDecoration': 'none', 'color': 'white', 'cursor': 'pointer', 'marginLeft': '10px' }} to='/login'><Button>Login</Button></Link>}
-            </div>
-        </Header>
-    )
-}
+  return (
+    <div className={style.header}>
+      <div className={style.header__logo}>Besocial!</div>
+      {isAuth ? (
+        <>
+          <div className={style.header__field}>
+            <div>{login}</div>
+            <button className="button button-logout" onClick={logoutCallback}>
+              Logout
+            </button>
+          </div>
+        </>
+      ) : (
+        <Link to="/login">
+          <button className="button button-login">Login</button>
+        </Link>
+      )}
+    </div>
+  );
+};
